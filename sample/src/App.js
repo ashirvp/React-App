bc31fda93dcaaea92bffc55ba2f35674b36f0114
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2021-11-10 09:13:11 +0530

    id chaged

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,27 +1,17 @@
+import Counter from "./Counter";
 import {useState} from 'react'
-import Employee from './Employee'
 function App() {
-  const [count,setCount]=useState(0)
-  const addCount=()=>{
-    setCount(count+1)
-    
-  }
-  let emp = [
-    { name: 'Ashir', age: 21 },
-    { name: 'Arun', age: 22 },
-    { name: 'Amal', age: 23 },
-  ]
+
+  const [state, setstate] = useState(false)
   return (
+
     <div className="App">
-      <button onClick={addCount}>Click</button>
-     {
-       emp.map((obj,index)=>{
-         return(
-          <Employee key={index} {...obj} />
-         )
-       })
-     }
+      <h1 onClick={()=>setstate(!state)}>Show / Hide</h1>
+      {state &&<Counter />}
+
     </div>
+
+
   );
 }
 
---
+++ sample/src/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Counter() {
+    
+    return (
+        <div>
+            <h1>Hello I'm a component</h1>
+        </div>
+    )
+}
+
+export default Counter
